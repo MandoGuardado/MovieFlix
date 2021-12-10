4a29f54220c3519ca59ac130ff6970d27f51897b
Mark Duenas <markgdlduenas@gmail.com>
2021-12-10 13:59:29 -0800

    attempted to fix stuff

--- src/components/MainDisplay/MainDisplay.js
+++ src/components/MainDisplay/MainDisplay.js
@@ -1,31 +1,28 @@
-
 import React from "react";
 import "./MainDisplay.css";
 import { Image } from "react-bootstrap";
+import MovieFlixlogo from "../../icons/MovieFlixlogo.png";
 
+function MainDisplay(props) {
+  const handleClick = () => {
+    console.log(props);
+  };
 
-function MainDisplay(props){
-
-    const handleClick = ()  => {
-        console.log(props);
-    }
-
-    return(
-        <>
-        <div className= 'container'>
-            <div className='display'>
-                <h1>Great movies, TV, Shows, and more!</h1>
-                <h5>Watch anywhere. For Free</h5>
-                <Image src="../../../MovieFlixlog.png" />
-        
-            </div>
-            <div>
-            <button onClick={handleClick} className='sign-in btn'>
+  return (
+    <>
+      <div className='container'>
+        <div className='display'>
+          <h1>Great movies, TV, Shows, and more!</h1>
+          <h5>Watch anywhere. For Free</h5>
+          <Image src={MovieFlixlogo} />
+        </div>
+        <div>
+          <button onClick={handleClick} className='sign-in btn'>
             Sign up!
-            </button>       
+          </button>
         </div>
-        </div>
-        </>
-    );
-};
+      </div>
+    </>
+  );
+}
 export default MainDisplay;
