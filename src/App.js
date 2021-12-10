20830f03269721d54117a9461f114c04822a7fbf
Mark Duenas <markgdlduenas@gmail.com>
2021-12-10 13:45:30 -0800

    added modal, and some card templates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Header from "./components/Header/Header";
 import "./firebase";
 import "./App.css";
-import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import SignUp from "./components/Form/SignUp";
 import SignIn from "./components/Form/SignIn";
@@ -10,7 +10,6 @@ import MainDisplay from "./components/MainDisplay/MainDisplay";
 import Dashboard from "./components/Dashboard/Dashboard";
 const App = () => {
   const navigate = useNavigate();
-
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
@@ -23,13 +22,11 @@ const App = () => {
 
   return (
     <div className='main-container'>
-
       <Header />
-    <MainDisplay/>
       <Routes>
+        <Route exact path='/' element={<MainDisplay />} />
         <Route exact path='/signup' element={<SignUp />} />
         <Route exact path='/signin' element={<SignIn />} />
-       
         <Route exact path='/dashboard' element={<Dashboard />} />
         {/* <Route path='*' element={<h1>NOT FOUND</h1>} /> */}
       </Routes>
