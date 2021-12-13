8b12ca9a0d0facce6c14fd5c50fd252b8c919d91
Armando Guardado <armando.guardado.ag@gmail.com>
2021-12-13 07:28:10 -0800

    VideoComponent to inject yout tube videos

---
+++ src/components/YouTube/Video.js
@@ -0,0 +1,25 @@
+import React from "react";
+import YouTube from "react-youtube";
+
+const VideoComponent = ({ id }) => {
+  const opts = {
+    height: "500",
+    width: "640",
+    // https://developers.google.com/youtube/player_parameters
+    //Values we can adjust to change how our videos displays
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      fs: 1,
+      modestbranding: 0
+    }
+  };
+
+  return (
+    <div className="video-component">
+      <YouTube videoId={id} opts={opts} />
+    </div>
+  );
+};
+
+export default VideoComponent;
